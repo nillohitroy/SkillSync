054e604a5c85835e1e348f49681540ea0410aa5c
Nillohit Roy <ai26.nillohit.roy@stcet.ac.in>
2026-05-24 20:58:21 +0530

    Added corsmiddleware origin for frontend website

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://skill-sync-one-delta.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
